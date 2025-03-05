77dc21bb74f7d4589839f5d58ca03cd067df56a1
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-03-06 01:00:36 +1100

    03/06/25 01:00:36

--- dot_scripts/executable_pdf.sh
+++ dot_scripts/executable_pdf.sh
@@ -2,7 +2,8 @@
 
 file=$1
 dir=$(dirname $file)
-name=${$(basename $file)%.*}
+name_ext=$(basename $file)
+name=${name_ext%.*}
 
 pandoc $file -o "$dir/$name.pdf" -V geometry:margin=3cm -V fontsize=12pt
 
