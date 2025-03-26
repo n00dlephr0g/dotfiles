a97491d2720418d311028c0c9e46642f097dd06b
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-03-26 11:23:14 +1100

    03/26/25 11:23:14

---
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- config here
+            })
+        end,
+    },
+}
