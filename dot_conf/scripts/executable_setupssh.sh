9582b181e43cdc0057f4ee52eb4653894fbc7f2f
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-04-09 09:35:59 +1000

    04/09/25 09:35:59

--- dot_conf/scripts/executable_setupssh.sh
+++ dot_conf/scripts/executable_setupssh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+ssh-keygen -t ed25519 -f ~/.ssh/scallop -N ""
 cat ~/.ssh/scallop.pub | ssh n00dlephr0g@scallop "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+
