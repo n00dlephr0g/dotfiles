4e2b054199bf9c6994bfcd463c20ca1bf278dec6
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-04-09 09:53:46 +1000

    04/09/25 09:53:46

--- dot_conf/scripts/executable_syncmusic.sh
+++ dot_conf/scripts/executable_syncmusic.sh
@@ -2,8 +2,13 @@
 
 SSID=$(iwgetid -r)
 IPADDR="scallop"
-if [ "$SSID" = "TimTen" ] then
+if [ "$SSID" = "TimTen" ]; then
+	echo "home network detected, syncing on local"
 	IPADDR="192.168.0.74"
+else
+	echo "no home network detected, syncing to remote"
 fi
+
+
 rsync -avh --info=progress2 ~/Music/ "n00dlephr0g@$IPADDR:/data/music"
 
