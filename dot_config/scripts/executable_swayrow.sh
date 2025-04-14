be2ccffe868a151296c8314e135dfb93e4f685b6
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-04-14 23:17:43 +1000

    04/14/25 23:17:43

--- dot_config/scripts/executable_swayrow.sh
+++ dot_config/scripts/executable_swayrow.sh
@@ -22,44 +22,44 @@ plusminus() {
 }
 
 changelogic() {
-# set argument variables for readability
-rc=$1
-operator=$2
-amount=$3
+    # set argument variables for readability
+    rc=$1
+    operator=$2
+    amount=$3
 
-# save current row to script
-oldws=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
-oldrow="${oldws%%:*}"
-oldcol="${oldws#*:}"
-savedir="/tmp/sway/swayrows"
-mkdir -p $savedir
+    # save current row to script
+    oldws=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
+    oldrow="${oldws%%:*}"
+    oldcol="${oldws#*:}"
+    savedir="/tmp/sway/swayrows"
+    mkdir -p $savedir
 
-if [[ "$rc" == "row" ]]; then
-    # set the new row
-    newrow=$(plusminus $oldrow $operator $amount)
-    # set the new col (load from save if exists)
-    if [[ -f "$savedir/$newrow" ]]; then
-        newcol=$(cat "$savedir/$newrow")
-    else
-        newcol=0
+    if [[ "$rc" == "row" ]]; then
+        # set the new row
+        newrow=$(plusminus $oldrow $operator $amount)
+        # set the new col (load from save if exists)
+        if [[ -f "$savedir/$newrow" ]]; then
+            newcol=$(cat "$savedir/$newrow")
+        else
+            newcol=0
+        fi
+        # save the old column into cache
+        echo "$oldcol" > "$savedir/$oldrow"
+        # return new workspace
+        echo "$newrow:$newcol"
+        # check if the original row still exists after switch and rm if not
+        if [[ -z $(swaymsg -t get_workspaces | jq -r '.[].name' | grep "$oldrow:") ]]; then
+            rm "$savedir/$oldrow"
+        fi
+        # done
+    elif [[ "$rc" == "col" ]]; then
+        # set the new col
+        newcol="$(plusminus $oldcol $operator $amount)"
+        # return new workspace
+        echo "$oldrow:$newcol"
     fi
-    # save the old column into cache
-    echo "$oldcol" > "$savedir/$oldrow"
-    # return new workspace
-    echo "$newrow:$newcol"
-    # check if the original row still exists after switch and rm if not
-    if [[ -z $(swaymsg -t get_workspaces | jq -r '.[].name' | grep "$oldrow:") ]]; then
-        rm "$savedir/$oldrow"
-    fi
-    # done
-elif [[ "$rc" == "col" ]]; then
-    # set the new col
-    newcol="$(plusminus $oldcol $operator $amount)"
-    # return new workspace
-    "$oldrow:$newcol"
-fi
 }
 
-swaymsg workspace $(changelogic)
+swaymsg workspace "$(changelogic $1 $2 $3)"
 
 
