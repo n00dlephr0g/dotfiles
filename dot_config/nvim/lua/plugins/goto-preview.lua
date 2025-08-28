8d568aa247163f03ef8fc0bb6ea69da8f41b68b2
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-08-28 13:23:34 +1000

    08/28/25 13:23:34

---
+++ dot_config/nvim/lua/plugins/goto-preview.lua
@@ -0,0 +1,7 @@
+return {
+    "rmagatti/goto-preview",
+    dependencies = { "rmagatti/logger.nvim" },
+    event = "BufEnter",
+    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+    default_mappings = true,
+}
