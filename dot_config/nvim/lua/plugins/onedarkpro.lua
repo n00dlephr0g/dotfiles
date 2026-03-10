abbc2cb97c9af934cc8c9fb4ac42d1cbe098a917
n00dlephr0g <monkeyjosephlee@gmail.com>
2026-03-10 14:11:13 +0000

    03/10/26 14:11:13

--- dot_config/nvim/lua/plugins/onedarkpro.lua
+++ dot_config/nvim/lua/plugins/onedarkpro.lua
@@ -3,17 +3,4 @@ return {
     "olimorris/onedarkpro.nvim",
     lazy = false,
     priority = 1000,
-    init = function()
-        require("onedarkpro").setup({
-            themes = {
-                pywal = "~/.config/nvim/lua/plugins/colors/pywal.lua",
-            },
-            styles = {
-                constants = "bold",
-            },
-        })
-        vim.cmd("OneDarkProClean")
-        vim.cmd("OneDarkProCache")
-        vim.cmd([[colorscheme pywal]])
-    end,
 }
